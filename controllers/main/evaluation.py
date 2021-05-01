37be95488c2b7687fce264a739aed638270c008f
Zhaoyuan Huo <zhaoyuanhuo@Arancini.local>
2021-05-01 15:26:16 -0400

    first commit

---
+++ controllers/main/evaluation.py
@@ -0,0 +1,96 @@
+# Please do not change this file
+
+import numpy as np
+from numpy import linalg as LA
+import math
+from util import closestNode
+
+def dist(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def avgDeltaDev(deltaVec):
+    deltaVec = np.array(deltaVec)
+    deltaVecDev = np.zeros((deltaVec.shape[0]-1,1))
+    for i in range(deltaVec.shape[0]-1):
+        deltaVecDev[i] = np.abs(deltaVec[i+1] - deltaVec[i])
+    avgDeltaDeviation = np.mean(deltaVecDev)
+    return avgDeltaDeviation
+
+def deltaGrader(avgDeltaDev, fs):
+    threshold = 0.025
+    if avgDeltaDev < threshold:
+        return fs
+    elif avgDeltaDev < 2.0*threshold:
+        return fs*0.5
+    else:
+        return 0
+        
+    
+def clGrader(traj, X, Y, fs, Cmax_cl):
+    ng = 0.0
+    ntrack = traj.shape[0]
+    XY = np.array([X,Y])
+    XY = XY.T
+    for i in range(ntrack):
+        minDist,_ = closestNode(traj[i,0], traj[i, 1], XY)
+        if minDist <= Cmax_cl:
+            ng += 1
+        else:
+            pass
+    return fs * (ng / float(ntrack))
+
+def adGrader(minDistList, fs, Cavg):
+    avg = np.average(minDistList)
+    if avg <= Cavg:
+        return fs
+    if avg <= Cavg*2:
+        return (-20/Cavg)*avg + 40
+    return 0
+
+def mdGrader(minDistList, fs, Cmax_md):
+    ng = 0
+    for i in range(len(minDistList)):
+        if minDistList[i] <= Cmax_md:
+            ng += 1
+    return fs*ng/len(minDistList)
+
+def beatBaselineGrader(timeCurrent, timeBaseline):
+    if timeCurrent <= timeBaseline:
+        return 10
+    elif timeCurrent <= 2.0*timeBaseline:
+        return 20 - 10*timeCurrent/timeBaseline
+    else:
+        return 0
+
+def evaluation(minDistList, traj_, X, Y, deltaVec):
+    print('Evaluating...')
+    avgDeltaDeviation = avgDeltaDev(deltaVec)
+    timeBaseline = 180
+    dt = 0.032
+    Cmax_cl = 15.0           # constraint of maximum distance for completing the loop
+    Cavg = 4.0              # constraint of average distance
+    Cmax_md = 8.0          # constraint of maximun distance
+    fs = 22.5              # the full score you can get
+    traj = traj_[1:len(traj_)-60,:]
+    comGrad = clGrader(traj, X, Y, fs, Cmax_cl)     # grade if you complete the loop
+    beatBaselineScore = 0.0
+
+    print('Score for completing the loop: {}/{}'.format(comGrad,fs))
+    avgGrad = adGrader(minDistList, fs, Cavg)                   # grade your average distance
+    print('Score for average distance: {}/{}'.format(avgGrad,fs))
+    maxGrad = mdGrader(minDistList, fs, Cmax_md)                # grade your maximum distance
+    print('Score for maximum distance: {}/{}'.format(maxGrad,fs))
+    delGrad = deltaGrader(avgDeltaDeviation, fs)               # grade your average deviation
+    print('Score for average delta fluctuation: {}/{}'.format(delGrad,fs))
+    
+    if comGrad < fs:
+        print('Your vehicle did not finish the loop.'
+              '\n You cannot enter the competition.')
+    else:
+        timeCurrent = len(X) * dt
+        beatBaselineScore = beatBaselineGrader(timeCurrent, timeBaseline)
+        print('Your time is ',
+              timeCurrent)
+
+    grade = avgGrad + maxGrad + comGrad + delGrad + beatBaselineScore
+    print("Your total score is : {}/100.0".format(grade))
---
+++ controllers/main/your_controller_ekf_slam.py
@@ -0,0 +1,159 @@
+# Fill in the respective function to implement the LQR/EKF SLAM controller
+
+# Import libraries
+import numpy as np
+from base_controller import BaseController
+from scipy import signal, linalg
+from scipy.spatial.transform import Rotation
+from util import *
+from ekf_slam import EKF_SLAM
+
+# CustomController class (inherits from BaseController)
+class CustomController(BaseController):
+
+    def __init__(self, trajectory):
+
+        super().__init__(trajectory)
+
+        # Define constants
+        # These can be ignored in P1
+        self.lr = 3.32
+        self.lf = 1.01
+        self.Ca = 20000
+        self.Iz = 29526.2
+        self.m = 4500
+        self.g = 9.81
+        
+        self.counter = 0
+        np.random.seed(99)
+
+        # Add additional member variables according to your need here.
+
+    def getStates(self, timestep, use_slam=False):
+
+        delT, X, Y, xdot, ydot, psi, psidot = super().getStates(timestep)
+
+        # Initialize the EKF SLAM estimation
+        if self.counter == 0:
+            # Load the map
+            minX, maxX, minY, maxY = -120., 450., -350., 50.
+            map_x = np.linspace(minX, maxX, 7)
+            map_y = np.linspace(minY, maxY, 7)
+            map_X, map_Y = np.meshgrid(map_x, map_y)
+            map_X = map_X.reshape(-1,1)
+            map_Y = map_Y.reshape(-1,1)
+            self.map = np.hstack((map_X, map_Y)).reshape((-1))
+            
+            # Parameters for EKF SLAM
+            self.n = int(len(self.map)/2)             
+            X_est = X + 0.5
+            Y_est = Y - 0.5
+            psi_est = psi - 0.02
+            mu_est = np.zeros(3+2*self.n)
+            mu_est[0:3] = np.array([X_est, Y_est, psi_est])
+            mu_est[3:] = np.array(self.map)
+            init_P = 1*np.eye(3+2*self.n)
+            W = np.zeros((3+2*self.n, 3+2*self.n))
+            W[0:3, 0:3] = delT**2 * 0.1 * np.eye(3)
+            V = 0.1*np.eye(2*self.n)
+            V[self.n:, self.n:] = 0.01*np.eye(self.n)
+            # V[self.n:] = 0.01
+            print(V)
+            
+            # Create a SLAM
+            self.slam = EKF_SLAM(mu_est, init_P, delT, W, V, self.n)
+            self.counter += 1
+        else:
+            mu = np.zeros(3+2*self.n)
+            mu[0:3] = np.array([X, 
+                                Y, 
+                                psi])
+            mu[3:] = self.map
+            y = self._compute_measurements(X, Y, psi)
+            mu_est, _ = self.slam.predict_and_correct(y, self.previous_u)
+
+        self.previous_u = np.array([xdot, ydot, psidot])
+
+        print("True      X, Y, psi:", X, Y, psi)
+        print("Estimated X, Y, psi:", mu_est[0], mu_est[1], mu_est[2])
+        print("-------------------------------------------------------")
+        
+        if use_slam == True:
+            return delT, mu_est[0], mu_est[1], xdot, ydot, mu_est[2], psidot
+        else:
+            return delT, X, Y, xdot, ydot, psi, psidot
+
+    def _compute_measurements(self, X, Y, psi):
+        x = np.zeros(3+2*self.n)
+        x[0:3] = np.array([X, Y, psi])
+        x[3:] = self.map
+        
+        p = x[0:2]
+        psi = x[2]
+        m = x[3:].reshape((-1,2))
+
+        y = np.zeros(2*self.n)
+
+        for i in range(self.n):
+            y[i] = np.linalg.norm(m[i, :] - p)
+            y[self.n+i] = wrapToPi(np.arctan2(m[i,1]-p[1], m[i,0]-p[0]) - psi)
+            
+        y = y + np.random.multivariate_normal(np.zeros(2*self.n), self.slam.V)
+        # print(np.random.multivariate_normal(np.zeros(2*self.n), self.slam.V))
+        return y
+
+    def update(self, timestep, driver):
+
+        trajectory = self.trajectory
+
+        lr = self.lr
+        lf = self.lf
+        Ca = self.Ca
+        Iz = self.Iz
+        m = self.m
+        g = self.g
+
+        # Fetch the states from the newly defined getStates method
+        delT, X, Y, xdot, ydot, psi, psidot = self.getStates(timestep, use_slam=True)
+
+        # You are free to reuse or refine your code from P3 in the spaces below.
+
+        # ---------------|Lateral Controller|-------------------------
+        """
+        Please design your lateral controller below.
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        """
+
+        # ---------------|Longitudinal Controller|-------------------------
+        """
+        Please design your longitudinal controller below.
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        .
+        """
+        
+        # Setting brake intensity is enabled by passing
+        # the driver object, which is used to provide inputs
+        # to the car, to our update function
+        # Using this function is purely optional.
+        # An input of 0 is no brakes applied, while
+        # an input of 1 is max brakes applied
+
+        # driver.setBrakeIntensity(clamp(someValue, 0, 1))
+
+        # Return all states and calculated control inputs (F, delta)
+        return X, Y, xdot, ydot, psi, psidot, F, delta
